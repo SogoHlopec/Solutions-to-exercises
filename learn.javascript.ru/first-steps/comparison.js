b8a267f8c0147feebb3d2861a5a04760171fb69e
SogoHlopec <sogo246@gmail.com>
2023-03-19 01:51:09 +0300

    feat: add solutions for first-steps

---
+++ learn.javascript.ru/first-steps/comparison.js
@@ -0,0 +1,23 @@
+/*
+Операторы сравнения
+важность: 5
+Каким будет результат этих выражений?
+
+5 > 4
+"ананас" > "яблоко"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+*/
+
+console.log(`
+5 > 4 = true
+"ананас" > "яблоко" = true
+"2" > "12" = false
+undefined == null = true
+undefined === null = false
+null == "\n0\n" = false
+null === +"\n0\n" = false
+`);
---
+++ learn.javascript.ru/first-steps/types.js
@@ -0,0 +1,16 @@
+/*
+Шаблонные строки
+важность: 5
+Что выведет этот скрипт?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+*/
+console.log("hello 1");
+console.log("hello name");
+console.log("hello Ilya");
