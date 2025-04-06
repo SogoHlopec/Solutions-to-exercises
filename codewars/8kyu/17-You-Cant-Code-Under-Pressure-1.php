4474a11798ad12c5677a66a82d655ea52f09c9c3
SogoHlopec <sogo246@gmail.com>
2025-04-06 18:00:04 +0300

    feat: add solution

---
+++ codewars/8kyu/17-You-Cant-Code-Under-Pressure-1.php
@@ -0,0 +1,22 @@
+<?
+
+/*
+https://www.codewars.com/kata/53ee5429ba190077850011d4
+You Can't Code Under Pressure #1
+
+Description:
+Code as fast as you can! You need to double the integer and return it.
+
+Tests:
+        class Test extends TestCase
+    {
+        public function testSampleTests() {
+        $this->assertSame(4, doubleInteger(2));
+        }
+    }
+
+*/
+
+function doubleInteger($i) {
+    return 2 * $i;
+}
